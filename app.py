09358e4df2a90e1d7bf9353967293862c04d3612
mituru-maker <mituru555uda@gmail.com>
2026-01-13 20:00:40 +0900

    色の変更

--- app.py
+++ app.py
@@ -22,6 +22,6 @@ quotes = [
 if st.button("新しい名言を受け取る"):
     selected_quote = random.choice(quotes)
     st.snow() # お祝いの風船
-    st.skeetball(selected_quote)
+    st.error(selected_quote)
 else:
     st.info("下のボタンをクリックしてください！")
